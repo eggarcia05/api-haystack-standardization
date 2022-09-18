a181550266f15c4aa0c1b8592afd76835a122ffb
eggarcia05 <erick.14.05.1998@gmail.com>
2022-09-18 16:14:14 -0500

    fix docs

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(function (err: any, req: Request, res: Response, next: any) {
 });
 
 // settings
-app.set("port", process.env.PORT || 8083);
+app.set("port", process.env.PORT || 8082);
 // app.set("portSocket", process.env.PORT_SOCKET || 8083);
 
 // middlewares
